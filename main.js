e52aa02fd52dc2d64d13550d4752762754bfa8bf
Davlat <davlatismatov2000@gmail.com>
2024-05-11 10:27:27 +0500

    modal

--- main.js
+++ main.js
@@ -10,3 +10,43 @@ const btn2 = document.querySelector(".btn2")
         close_Btn.addEventListener("click", () =>{
             container.classList.remove("active")
         })
+
+
+
+        const scroller = document.querySelector("#scroller");
+        const output = document.querySelector("#output");
+        
+        scroller.addEventListener("scroll", (event) => {
+          output.textContent = `scrollTop: ${scroller.scrollTop}`;
+        });
+
+
+const data = [
+  
+  {id: 1, name : "Davlat", last : "Ismatov", kurs : "front-end",  datasi : "Jan", status: "done"},
+  {id: 2, name : "Davlat", last : "Ismatov", kurs : "front-end",  datasi : "Jan", status: "done"},
+  {id: 3, name : "Davlat", last : "Ismatov", kurs : "front-end",  datasi : "Jan", status: "done"},
+  {id: 4, name : "Davlat", last : "Ismatov", kurs : "front-end",  datasi : "Jan", status: "done"},
+  {id: 5, name : "Davlat", last : "Ismatov", kurs : "front-end",  datasi : "Jan", status: "done"},
+  {id: 6, name : "Davlat", last : "Ismatov", kurs : "front-end",  datasi : "Jan", status: "done"},
+  {id: 7, name : "Davlat", last : "Ismatov", kurs : "front-end",  datasi : "Jan", status: "done"},
+  {id: 8, name : "Davlat", last : "Ismatov", kurs : "front-end",  datasi : "Jan", status: "done"},
+  {id: 9, name : "Davlat", last : "Ismatov", kurs : "front-end",  datasi : "Jan", status: "done"},
+  {id: 10, name : "Davlat", last : "Ismatov", kurs : "front-end",  datasi : "Jan", status: "done"}
+  ]
+// console.log(arr);
+
+const tbody = document.getElementById("table_tbody")
+
+data.forEach(element => {
+  tbody.innerHTML += `
+  <tr>
+  <th scope="row">${element.id}</th>
+  <td colspan="2">${element.name}</td>
+  <td>${element.last}</td>
+  <td>${element.kurs}</td>
+  <td>${element.datasi}</td>
+  <th>${element.status}</th>
+</tr>
+  `
+});
