24000c4f9e2a88ae66d8ec594519af7a5120167d
Davlat <davlatismatov2000@gmail.com>
2024-05-14 10:35:30 +0500

    project

--- main.js
+++ main.js
@@ -23,16 +23,16 @@ const btn2 = document.querySelector(".btn2")
 
 const data = [
   
-  {id: 1, name : "Davlat", last : "Ismatov", kurs : "front-end",  datasi : "Jan", status: "done", delet: "o'chirish"},
-  {id: 2, name : "Davlat", last : "Ismatov", kurs : "front-end",  datasi : "Jan", status: "done"},
-  {id: 3, name : "Davlat", last : "Ismatov", kurs : "front-end",  datasi : "Jan", status: "done"},
-  {id: 4, name : "Davlat", last : "Ismatov", kurs : "front-end",  datasi : "Jan", status: "done"},
-  {id: 5, name : "Davlat", last : "Ismatov", kurs : "front-end",  datasi : "Jan", status: "done"},
-  {id: 6, name : "Davlat", last : "Ismatov", kurs : "front-end",  datasi : "Jan", status: "done"},
-  {id: 7, name : "Davlat", last : "Ismatov", kurs : "front-end",  datasi : "Jan", status: "done"},
-  {id: 8, name : "Davlat", last : "Ismatov", kurs : "front-end",  datasi : "Jan", status: "done"},
-  {id: 9, name : "Davlat", last : "Ismatov", kurs : "front-end",  datasi : "Jan", status: "done"},
-  {id: 10, name : "Davlat", last : "Ismatov", kurs : "front-end",  datasi : "Jan", status: "done"}
+  {id: 1, name : "Davlat", last : "Ismatov", kurs : "front-end",  datasi : "Jan", status: "yaxshi", delet: "o'chirish"},
+  {id: 2, name : "Davlat", last : "Ismatov", kurs : "front-end",  datasi : "Jan", status: "qoniqarli", delet: "o'chirish"},
+  {id: 3, name : "Davlat", last : "Ismatov", kurs : "front-end",  datasi : "Jan", status: "qoniqarsiz", delet: "o'chirish"},
+  {id: 4, name : "Davlat", last : "Ismatov", kurs : "front-end",  datasi : "Jan", status: "yaxshi", delet: "o'chirish"},
+  {id: 5, name : "Davlat", last : "Ismatov", kurs : "front-end",  datasi : "Jan", status: "qoniqarli", delet: "o'chirish"},
+  {id: 6, name : "Davlat", last : "Ismatov", kurs : "front-end",  datasi : "Jan", status: "qoniqarsiz", delet: "o'chirish"},
+  {id: 7, name : "Davlat", last : "Ismatov", kurs : "front-end",  datasi : "Jan", status: "yaxshi", delet: "o'chirish"},
+  {id: 8, name : "Davlat", last : "Ismatov", kurs : "front-end",  datasi : "Jan", status: "qoniqarli", delet: "o'chirish"},
+  {id: 9, name : "Davlat", last : "Ismatov", kurs : "front-end",  datasi : "Jan", status: "qoniqarsiz", delet: "o'chirish"},
+  {id: 10, name : "Davlat", last : "Ismatov", kurs : "front-end",  datasi : "Jan", status: "yaxshi", delet: "o'chirish"}
   ]
 // console.log(arr);
 
@@ -47,7 +47,7 @@ data.forEach(element => {
   <td>${element.kurs}</td>
   <td>${element.datasi}</td>
   <th>${element.status}</th>
-  <td><button>${element.delet}</button></td>
+  <td><button type="button" class="btn btn-danger">${element.delet}</button></td>
 </tr>
   `
 });
