b8967d9589d928fa29821dc7dff09d177c1d3c03
Davlat <davlatismatov2000@gmail.com>
2024-05-21 08:48:40 +0500

    izzat

--- main.js
+++ main.js
@@ -51,3 +51,73 @@ data.forEach(element => {
 </tr>
   `
 });
+
+
+
+// Ma'lumotlarni localStorage ga yozish
+function saveDataToLocalStorage(username, email) {
+  var users = JSON.parse(localStorage.getItem('users')) || [];
+  users.push({ username: username, email: email });
+  localStorage.setItem('users', JSON.stringify(users));
+  console.log(localStorage.getItem('users'))
+}
+
+// Ma'lumotlarni localStorage dan o'qish va jadvalga chiqarish
+function loadDataFromLocalStorage() {
+  var users = JSON.parse(localStorage.getItem('users')) || [];
+  var table = document.getElementById('userTable').getElementsByTagName('tbody')[0];
+  table.innerHTML = '';
+  users.forEach(function(user) {
+      var newRow = table.insertRow(table.rows.length);
+      var cell1 = newRow.insertCell(0);
+      var cell2 = newRow.insertCell(1);
+      var cell3 = newRow.insertCell(2);
+      
+      cell1.innerHTML = user.username;
+      cell2.innerHTML = user.email;
+      cell3.innerHTML = '<button onclick="editUser(this)">Edit</button> <button onclick="deleteUser(this)">Delete</button>';
+  });
+}
+
+// Foydalanuvchi qo'shish
+function addUser(event) {
+  event.preventDefault();
+  var username = document.getElementById('username').value;
+  var email = document.getElementById('email').value;
+  
+  if (username && email) {
+      saveDataToLocalStorage(username, email);
+      loadDataFromLocalStorage();
+      
+      document.getElementById('username').value = '';
+      document.getElementById('email').value = '';
+  } else {
+      alert('Please fill out all fields');
+  }
+}
+
+// Foydalanuvchi o'zgartirish
+function editUser(row) {
+  var selectedRow = row.parentNode.parentNode;
+  var username = selectedRow.cells[0].innerHTML;
+  var email = selectedRow.cells[1].innerHTML;
+  
+  document.getElementById('username').value = username;
+  document.getElementById('email').value = email;
+  
+  selectedRow.remove();
+}
+
+// Foydalanuvchi o'chirish
+function deleteUser(row) {
+  var selectedRow = row.parentNode.parentNode;
+  var username = selectedRow.cells[0].innerHTML;
+  var email = selectedRow.cells[1].innerHTML;
+  var users = JSON.parse(localStorage.getItem('users')) || [];
+  var updatedUsers = users.filter(function(user) {
+      return user.username !== username || user.email !== email;
+  });
+  localStorage.setItem('users', JSON.stringify(updatedUsers));
+  
+  loadDataFromLocalStorage();
+}
